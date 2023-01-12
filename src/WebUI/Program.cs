0dadcf61e553466e62bbd8b642ca86adc84841dd
Brandy Kundenga <55144997+BrandyKun@users.noreply.github.com>
2023-01-12 19:58:34 +0000

    changes deom files and added migrations

--- src/WebUI/Program.cs
+++ src/WebUI/Program.cs
@@ -1,7 +1,12 @@
+using Gaming.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllersWithViews();
 builder.Services.AddSwaggerDocument();
 
