784d7ee447e0b40db6d711a2fe5b35d67b9b9d7e
Brandy Kundenga <55144997+BrandyKun@users.noreply.github.com>
2023-01-17 08:54:10 +0000

    dto changes

--- src/Application/RequestBodyDto.cs
+++ src/Application/RequestBodyDto.cs
@@ -4,6 +4,7 @@ namespace Application
     {
         public string EndPoint { get; set; }
         public string Query { get; set; }
+        public string? Date {get;set;}
         public int Limit { get; set; }
     }
 }
