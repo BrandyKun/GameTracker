f835990f210a3de194150bdee55c3bc4e8b3b986
Brandy Kundenga <55144997+BrandyKun@users.noreply.github.com>
2023-01-22 19:21:24 +0000

    initali carousel home

--- src/WebUI/ClientApp/src/components/Home.js
+++ src/WebUI/ClientApp/src/components/Home.js
@@ -1,5 +1,12 @@
 import React from "react";
-import Dropdown from "./UI/Dropdown";
+import { Form } from "reactstrap";
+
+import {Swiper, SwiperSlide} from "swiper/react";
+import { EffectCoverflow, Pagination } from "swiper";
+
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
 
 const Home = () => {
 
@@ -13,9 +20,52 @@ const Home = () => {
       <div>Home</div>
       <div className="main">
         <nav className="filters">
-          <Dropdown placeHolder="Select" options={options}/>
         </nav>
         <div className="games-container">
+        <Swiper
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={"auto"}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-1.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-2.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-3.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-4.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-5.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-6.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-7.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-8.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src="https://swiperjs.com/demos/images/nature-9.jpg" />
+        </SwiperSlide>
+      </Swiper>
           <div className="game.page">
             <img src="" alt="" />
             <div className="game_title"></div>
--- src/WebUI/ClientApp/src/components/Layout.js
+++ src/WebUI/ClientApp/src/components/Layout.js
@@ -9,9 +9,9 @@ export class Layout extends Component {
     return (
       <div>
         <NavMenu />
-        <Container tag="main">
+        <div>
           {this.props.children}
-        </Container>
+        </div>
       </div>
     );
   }
