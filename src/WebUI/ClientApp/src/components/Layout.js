e8c6e99c49713d1ad8d63dfdcd4e1696d9460c1c
Brandy Kundenga <55144997+BrandyKun@users.noreply.github.com>
2023-03-11 01:30:19 +0000

    nav and login page

--- src/WebUI/ClientApp/src/components/Layout.js
+++ src/WebUI/ClientApp/src/components/Layout.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
 import Footer from './Footer';
-import { NavMenu } from './NavMenu';
+import NavMenu from './NavMenu';
 
 export class Layout extends Component {
   static displayName = Layout.name;
