1b00569f6cf810374848e52e32689960cff12805
Brandy Kundenga <55144997+BrandyKun@users.noreply.github.com>
2023-05-09 05:05:25 +0100

    minor stuff

--- src/WebUI/ClientApp/src/components/Footer.js
+++ src/WebUI/ClientApp/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div style={{width:'100%', height:'400px', backgroundColor: 'blue' }}>Footer</div>
+    <div style={{width:'100%', height:'400px', backgroundColor: '#181A24' }}>Footer</div>
   )
 }
 
